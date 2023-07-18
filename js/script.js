0d661f7e249271b49dd5608fad4fa41459102dcc
First Guardian <raflisugiarto2004@gmail.com>
2023-07-18 15:33:29 +0700

    Add navbar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 feather.replace();
+
+const navbarNav = document.getElementById('navbar-nav');
+const navbarToggler = document.getElementById('navbar-toggler');
+const mainContent = document.getElementById('main-content');
+const toggleNavbar = () => {
+  navbarNav.classList.toggle('navbar-nav--collapse');
+};
+const closeNavbar = () => {
+  navbarNav.classList.remove('navbar-nav--collapse');
+};
+
+navbarToggler.addEventListener('click', toggleNavbar);
+mainContent.addEventListener('click', closeNavbar);
