f6cb59283ac399e2aa8a36fccabb1bb83823c545
First Guardian <raflisugiarto2004@gmail.com>
2023-07-26 15:29:57 +0700

    Menu section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+// Icon
 feather.replace();
 
+// Navbar
 const navbarNav = document.getElementById('navbar-nav');
 const navbarToggler = document.getElementById('navbar-toggler');
 const mainContent = document.getElementById('main-content');
@@ -12,17 +14,3 @@ const closeNavbar = () => {
 
 navbarToggler.addEventListener('click', toggleNavbar);
 mainContent.addEventListener('click', closeNavbar);
-
-// About's image
-// const aboutImg = document.getElementById('about-img');
-// const imgMouseOver = () => {
-//   aboutImg.classList.remove('main-img--animate-1');
-// };
-// const imgMouseOut = () => {
-//   setTimeout(() => {
-//     aboutImg.classList.add('main-img--animate-1');
-//   }, 500);
-// };
-
-// aboutImg.addEventListener('mouseover', imgMouseOver);
-// aboutImg.addEventListener('mouseout', imgMouseOut);
